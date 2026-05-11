908a88f4e9b50a92bf4e28f24b4e754947b67969
Mawiel <andreasnordboe@gmail.com>
2026-05-11 11:09:14 +0100

    Implemented robot move and get statuses for the robot api service on the backend

--- RobotManagementSystem/Services/Robot/RobotApiService.cs
+++ RobotManagementSystem/Services/Robot/RobotApiService.cs
@@ -1,17 +1,78 @@
+using RobotManagementSystem.Shared.Models.Components;
 using RobotManagementSystem.Shared.Models.Robot;
 
 namespace RobotManagementSystem.Services;
 
 public class RobotApiService : IRobotApiService
 {
-    public Task<RobotStatusResponse?> GetRobotStatusAsync()
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<RobotApiService> _logger;
+    
+    public async Task<RobotStatusResponse?> GetRobotStatusAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            return await _httpClient.GetFromJsonAsync<RobotStatusResponse>("/api/status");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Could not get robot status.");
+            return null;
+        }
     }
 
-    public Task<RobotCommandResponse?> MoveRobotAsync(RobotNavigationRequest request)
+    public async Task<RobotCommandResponse?> MoveRobotAsync(RobotNavigationRequest request)
     {
-        throw new NotImplementedException();
+        // Validate coordinates so they don't go out of bounds (TODO move this to helper later)
+        if (request.X < 0 || request.Y < 0 || request.X > 20 || request.Y > 20)
+        {
+            return new RobotCommandResponse
+            {
+                Success = false,
+                Message = "Robot coordinates not valid and must be between 0 and 20."
+            };
+        }
+
+        try
+        {
+            var moveRequest = new RobotNavigationRequest
+            {
+                X = request.X,
+                Y = request.Y
+            };
+            
+            var response = await _httpClient.PostAsJsonAsync("/api/move", moveRequest);
+            if (!response.IsSuccessStatusCode)
+            {
+                return new RobotCommandResponse
+                {
+                    Success = true,
+                    Message = $"Move command failed. Response: {response.StatusCode}." // TODO I'll try stautus code for now and try ReasonPhrase later 
+                };
+            }
+            
+            return new RobotCommandResponse
+            {
+                Success = true,
+                Message = $"Robot was moved to {moveRequest.X}, {moveRequest.Y}.",
+                RobotPosition = new Vector2D
+                {
+                    X = moveRequest.X,
+                    Y = moveRequest.Y
+                }
+            };
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Robot move command failed.");
+            
+            return new RobotCommandResponse
+            {
+                Success = false,
+                Message = "Robot move command failed."
+            };
+        }
     }
 
     public Task<RobotCommandResponse?> ResetAsync()
